4124fd1423dc699fadc005b5c4f35fe7248a399b
storm <17706634347@163.com>
2018-07-10 09:31:55 +0800

    1.slot 作用域插槽 2.promise bluebird 3.iview组件demo

---
+++ src/views/slotScope.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+    div
+       slotParent 
+</template>
+<script>
+
+import slotParent from '../components/slot_parent';
+export default {
+    components: { slotParent }
+}
+</script>
